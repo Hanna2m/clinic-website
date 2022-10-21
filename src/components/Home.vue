8fb062ad11357239c42e11f4eff6771bc83874bb
Hanna Mazur <hanna2mazur@gmail.com>
2022-10-21 14:51:27 +0200

    init commit

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <button class="btn">Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
