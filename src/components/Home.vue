f3997160f57382e7c4e4763bf16339ee4e3bf436
Hanna Mazur <hanna2mazur@gmail.com>
2022-11-01 14:19:17 +0100

    add responsive navbar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,59 @@
 <template>
-  <div class="hello">
-    <button class="btn">Click me</button>
+  <div>
+    <nav>
+      <div class="container">
+        <a href="" class="nav-logo">
+          <h3>Your Doctor</h3>
+        </a>
+
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#specialists">Specialists</a></li>
+          <li><a href="#testimonials">Testimonials</a></li>
+          <li><a class="btn" href="#appointment">Make Appointment</a></li>
+        </ul>
+
+        <ul class="nav-items_mobile"
+            :class="this.showMobileMenu ? 'open-menu' : 'close-menu'">
+          <li><a href="#">Home</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#specialists">Specialists</a></li>
+          <li><a href="#testimonials">Testimonials</a></li>
+          <li><a class="btn" href="#appointment">Make Appointment</a></li>
+        </ul>
+        <div v-if="showMobileMenu == true">
+          <button id="open_nav-btn" @click="showMenu"><box-icon name='menu' color="#ffffff"/></button>
+        </div>
+        <div v-if="showMobileMenu == false">
+          <button id="close_nav-btn" @click="showMenu"><box-icon name='x' color="#ffffff" ></box-icon></button>
+        </div>
+        
+      </div>
+    </nav>
   </div>
 </template>
 
 <script>
+import 'boxicons';
+
 export default {
   name: 'Home',
-  props: {
-    msg: String
+
+  data() {
+    return {
+      showMobileMenu: true
+    }
+  },
+
+  methods: {
+    showMenu() {
+      this.showMobileMenu = !this.showMobileMenu
+    }
   }
 }
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
