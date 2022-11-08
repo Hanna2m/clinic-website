538fcfe3560f5db7bb8761e517d5af5017aa96af
Hanna Mazur <hanna2mazur@gmail.com>
2022-11-08 09:36:34 +0100

    add specialists and testimonials section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,23 +22,270 @@
           <li><a href="#testimonials">Testimonials</a></li>
           <li><a class="btn" href="#appointment">Make Appointment</a></li>
         </ul>
-        <div v-if="showMobileMenu == true">
-          <button id="open_nav-btn" @click="showMenu"><box-icon name='menu' color="#ffffff"/></button>
-        </div>
-        <div v-if="showMobileMenu == false">
-          <button id="close_nav-btn" @click="showMenu"><box-icon name='x' color="#ffffff" ></box-icon></button>
-        </div>
-        
+
+        <button v-if="showMobileMenu == true" id="open_nav-btn" @click="showMenu"><box-icon name='menu' color="#ffffff"/></button>
+        <button v-if="showMobileMenu == false" id="close_nav-btn" @click="showMenu"><box-icon name='x' color="#ffffff" ></box-icon></button>
+      
       </div>
     </nav>
+
+    <header>
+      <div class="container">
+        <div class="header-content">
+          <h1>Complete Health Care Solutions</h1>
+          <p>Start taking care of your health today. Advanced technologies of preventive medicine for your well-being.</p>
+          <a class="btn" href="#appointment">Make Appointment</a>
+        </div>
+        <div class="header-image">
+          <img src="../assets/fem_doc_img.jpg" alt="header image"/>
+        </div>
+      </div>
+    </header>
+
+    <section id="services">
+      <h2>Our services</h2>
+      <p>Our exelent team works for you</p>
+      <div class="container">
+        <article>
+          <i class='bx bx-user'/>
+          <div>
+            <h4>Online  Booking</h4>
+            <body-2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat explicabo dolor rem eligendi fugit tempore.</body-2>
+          </div>
+        </article>
+
+        <article>
+          <i class='bx bx-trophy'/>
+          <div>
+            <h4>Qualified Doctors</h4>
+            <body-2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat explicabo dolor rem eligendi fugit tempore.</body-2>
+          </div>
+        </article>
+
+        <article>
+          <i class='bx bx-support' />
+          <div>
+            <h4>Privat support</h4>
+            <body-2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat explicabo dolor rem eligendi fugit tempore.</body-2>
+          </div>
+        </article>
+
+        <article>
+          <i class='bx bx-donate-heart' />
+          <div>
+            <h4>Well Cared</h4>
+            <body-2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat explicabo dolor rem eligendi fugit tempore.</body-2>
+          </div>
+        </article>
+
+        <article>
+          <i class='bx bx-first-aid' />
+          <div>
+            <h4>Need Emergency</h4>
+            <body-2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat explicabo dolor rem eligendi fugit tempore.</body-2>
+          </div>
+        </article>
+
+        <article>
+          <i class='bx bxl-meta' />
+          <div>
+            <h4>24 Hour Service</h4>
+            <body-2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat explicabo dolor rem eligendi fugit tempore.</body-2>
+          </div>
+        </article>
+
+      </div>
+    </section>
+
+    <section id="specialists">
+      <h2>Our Top Specialists</h2>
+      <p>Our exelent team works for you</p>
+      <div class="container">
+        <article>
+          <div class="specialist-img">
+            <img src="../assets/specialist-1.png" alt="specialist1"/>
+          </div>
+          <div class="specialist-details">
+            <h4>Dr. Annika Smith</h4>
+            <body-2>Surgery Specialist</body-2>
+          </div>
+          <div class="specialist-socials">
+            <a href="https://linkedin.com" target="_blank"><i class='bx bxl-linkedin' /></a>
+            <a href="https://twitter.com" target="_blank"><i class='bx bxl-twitter' /></a>
+            <a href="https://facebook.com" target="_blank"><i class='bx bxl-facebook' /></a>
+          </div>
+          <a href="https://api.whatsapp.com/send?phone=3197010240285" class="specialist-whatsapp" target="_blank"><i class='bx bxl-whatsapp' /></a>
+        </article>
+
+        <article>
+          <div class="specialist-img">
+            <img src="../assets/specialist-2.png" alt="specialist2"/>
+          </div>
+          <div class="specialist-details">
+            <h4>Dr. John Malkovich</h4>
+            <body-2>Surgery Specialist</body-2>
+          </div>
+          <div class="specialist-socials">
+            <a href="https://linkedin.com" target="_blank"><i class='bx bxl-linkedin' /></a>
+            <a href="https://twitter.com" target="_blank"><i class='bx bxl-twitter' /></a>
+            <a href="https://facebook.com" target="_blank"><i class='bx bxl-facebook' /></a>
+          </div>
+          <a href="https://api.whatsapp.com/send?phone=3197010240285" class="specialist-whatsapp" target="_blank"><i class='bx bxl-whatsapp' /></a>
+        </article>
+
+        <article>
+          <div class="specialist-img">
+            <img src="../assets/specialist-3.png" alt="specialist3"/>
+          </div>
+          <div class="specialist-details">
+            <h4>Dr. Nika Dubrowsky</h4>
+            <body-2>Surgery Specialist</body-2>
+          </div>
+          <div class="specialist-socials">
+            <a href="https://linkedin.com" target="_blank"><i class='bx bxl-linkedin' /></a>
+            <a href="https://twitter.com" target="_blank"><i class='bx bxl-twitter' /></a>
+            <a href="https://facebook.com" target="_blank"><i class='bx bxl-facebook' /></a>
+          </div>
+          <a href="https://api.whatsapp.com/send?phone=3197010240285" class="specialist-whatsapp" target="_blank"><i class='bx bxl-whatsapp' /></a>
+        </article>
+
+        <article>
+          <div class="specialist-img">
+            <img src="../assets/specialist-4.png" alt="specialist4"/>
+          </div>
+          <div class="specialist-details">
+            <h4>Dr. Michelle Morrone</h4>
+            <body-2>Surgery Specialist</body-2>
+          </div>
+          <div class="specialist-socials">
+            <a href="https://linkedin.com" target="_blank"><i class='bx bxl-linkedin' /></a>
+            <a href="https://twitter.com" target="_blank"><i class='bx bxl-twitter' /></a>
+            <a href="https://facebook.com" target="_blank"><i class='bx bxl-facebook' /></a>
+          </div>
+          <a href="https://api.whatsapp.com/send?phone=3197010240285" class="specialist-whatsapp" target="_blank"><i class='bx bxl-whatsapp' /></a>
+        </article>
+      </div>
+    </section>
+
+    <section id="testimonials">
+      <h2>What Our Patients Say </h2>
+      <p>Check reviews from our patients</p>
+
+      <div class="container">
+
+        <swiper
+          :slidesPerView="1"
+          :spaceBetween="30"
+          :pagination="{
+            clickable: true,
+          }"
+          :modules="modules"
+          :breakpoints="{
+            600: {
+              slidesPerView: 2
+            },
+            1024: {
+              slidesPerView: 3
+            }
+          }"
+          class="mySwiper"
+        >
+          <swiper-slide>
+            <article>
+              <body-2>Lorem ipsum dolor sit, amet consectetur adipisicing elit.bcaecati quisquam unde eveniet libero quod nobis necessitatibus omnis sunt. Perspiciatis, ipsa.</body-2>
+              <div class="patient">
+                <div class="avatar">
+                  <img src="../assets/person1.jpg" alt="patient1">
+                </div>
+                <div class="patient-details">
+                  <h5>Jina Markus</h5>
+                  <caption>Patient</caption>
+                </div>
+              </div>
+            </article>
+          </swiper-slide>
+          <swiper-slide>
+            <article>
+              <body-2>Lorem ipsum dolor sit, amet consectetur adipisicing elit.bcaecati quisquam unde eveniet libero quod nobis necessitatibus omnis sunt. Perspiciatis, ipsa.</body-2>
+              <div class="patient">
+                <div class="avatar">
+                  <img src="../assets/person2.jpg" alt="patient2">
+                </div>
+                <div class="patient-details">
+                  <h5>Mike Simons</h5>
+                  <caption>Patient</caption>
+                </div>
+              </div>
+            </article>
+          </swiper-slide>
+          <swiper-slide>
+            <article>
+              <body-2>Lorem ipsum dolor sit, amet consectetur adipisicing elit.bcaecati quisquam unde eveniet libero quod nobis necessitatibus omnis sunt. Perspiciatis, ipsa.</body-2>
+              <div class="patient">
+                <div class="avatar">
+                  <img src="../assets/icon_female.svg" alt="patient3">
+                </div>
+                <div class="patient-details">
+                  <h5>Margo Jones</h5>
+                  <caption>Patient</caption>
+                </div>
+              </div>
+            </article>
+          </swiper-slide>
+          <swiper-slide>
+            <article>
+              <body-2>Lorem ipsum dolor sit, amet consectetur adipisicing elit.bcaecati quisquam unde eveniet libero quod nobis necessitatibus omnis sunt. Perspiciatis, ipsa.</body-2>
+              <div class="patient">
+                <div class="avatar">
+                  <img src="../assets/person4.jpeg" alt="patient4">
+                </div>
+                <div class="patient-details">
+                  <h5>Dina Marina</h5>
+                  <caption>Patient</caption>
+                </div>
+              </div>
+            </article>
+          </swiper-slide>
+          <swiper-slide>
+            <article>
+              <body-2>Lorem ipsum dolor sit, amet consectetur adipisicing elit.bcaecati quisquam unde eveniet libero quod nobis necessitatibus omnis sunt. Perspiciatis, ipsa.</body-2>
+              <div class="patient">
+                <div class="avatar">
+                  <img src="../assets/icon_male.svg" alt="patient5">
+                </div>
+                <div class="patient-details">
+                  <h5>Jack Toffel</h5>
+                  <caption>Patient</caption>
+                </div>
+              </div>
+            </article>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import 'boxicons';
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
 
 export default {
   name: 'Home',
+
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+
+  setup() {
+    return {
+      modules: [Pagination],
+    };
+  },
 
   data() {
     return {
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import '@/style/main.scss';
+import 'boxicons/css/boxicons.min.css'
 
 createApp(App).mount('#app')
